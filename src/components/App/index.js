ead2acbe9f4c2571d15215b7488f21b61228cb85
Jonathan Lester <jonathanmarklester@gmail.com>
2019-03-27 15:01:22 +0000

    conditional rendering of login page working but... I had to change a button back to input

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,62 +12,119 @@ import styled from "styled-components";
 import DnDTest from "../DnD/DnDTest";
 
 const AppWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 100px;
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
 `;
 
 const Container = styled.div``;
 
 class App extends React.Component {
-  render() {
-    return (
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: Boolean(localStorage.getItem("token")),
+            email: "",
+            password: ""
+        };
+    }
 
-      <div>
-        <div className="App">
-          <NavBar />
-          <div className="container">
-            <div className="listCont">
-              <div>
-                <UserList />
-                <Clock />
-              </div>
-              <div>
-                <StaffList />
-              </div>
+    onChange = event => {
+        const { name, value } = event.target;
+        this.setState(state => ({
+            [name]: value
+        }));
+    };
 
-            </div>
-            <div className="gridCont">
-              <div className="Grid">
-                <Grid />
-              </div>
-            </div>
-          </div>
-        </div>
-        <br />
-        <br />
-        <div>
-          <div>
-            <LogInHeader />
-          </div>
-          <div>
-            <Logo />
-          </div>
-          <div className="logInCont">
-            <LogInContainer />
-          </div>
-          <div>
-            <h6>WTP © 2019</h6>
-          </div>
-        </div>
-        <AppWrapper>
-          <Container>
-            <DnDTest />
-          </Container>
-        </AppWrapper>
-      </div>
-    );
-  }
+    onLogin = event => {
+        event.preventDefault();
+        fetch(`http://localhost:5000/authenticate`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        })
+            .then(res => res.json())
+            .then(({ message, success, token }) => {
+                if (success && token) {
+                    localStorage.setItem("token", token);
+                    this.setState(state => ({
+                        isLoggedIn: true
+                    }));
+                }
+                console.log(message);
+            })
+            .then(
+                this.setState(state => ({
+                    email: "",
+                    password: ""
+                }))
+            )
+            .catch(err => console.error(err));
+        //.finally(() => this.setState(() => ({ isLoading: false })));
+    };
+
+    render() {
+        return (
+            <>
+                {!this.state.isLoggedIn ? (
+                    <div>
+                        <div>
+                            <LogInHeader />
+                        </div>
+                        <div>
+                            <Logo />
+                        </div>
+                        <div className="logInCont">
+                            <LogInContainer
+                                onLogin={this.onLogin}
+                                onChange={this.onChange}
+                                email={this.state.email}
+                                password={this.state.password}
+                            />
+                        </div>
+                        <div>
+                            <h6>WTP © 2019</h6>
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <div className="App">
+                            <NavBar />
+                            <div className="container">
+                                <div className="listCont">
+                                    <div>
+                                        <UserList />
+                                        <Clock />
+                                    </div>
+                                    <div>
+                                        <StaffList />
+                                    </div>
+                                </div>
+                                <div className="gridCont">
+                                    <div className="Grid">
+                                        <Grid />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                        <br />
+                        {/* <AppWrapper>
+                        <Container>
+                            <DnDTest />
+                        </Container>
+                    </AppWrapper> */}
+                    </div>
+                )}
+            </>
+        );
+    }
 }
 
 export default App;
