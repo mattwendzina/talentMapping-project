15c7fad471d1c369cd725a140cb71c61eb497c5b
SahelaR <sahelarani1@gmail.com>
2019-03-27 13:47:58 +0000

    loginPage

---
+++ src/components/Logo/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../santander.png";
+import Button from "@material-ui/core/Button";
+import Styles from "./Logo.module.css";
+
+class Logo extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className={Styles.left}>
+          <img
+            src={logo}
+            alt="Santander"
+            style={{ width: "220px", height: "80px" }}
+          />
+
+          <div className={Styles.right}>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="small"
+              type="submit"
+            >
+              Staff Intranet
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Logo;
