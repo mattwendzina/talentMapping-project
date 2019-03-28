7fd3b35de7959b338f2aced7e94a92f9dee5f737
unknown <rhysh1986@gmail.com>
2019-03-28 13:59:22 +0000

    hopefullly resolved merge conflicts

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -22,83 +22,130 @@ class Board extends React.Component {
       value: "",
       users2: [],
       testUsers: [
-                {
-                    user: "7654redxcvgyu789olmnbvfr",
-                    firstName: "Jonny",
-                    lastName: "Lester",
-                    position: 0
-                },
-                {
-                    user: "plkjhgfder45678ir",
-                    firstName: "Rhys",
-                    lastName: "Handley",
-                    position: 0
-                },
-                {
-                    user: "0987654esxcvbhjk",
-                    firstName: "Sahela",
-                    lastName: "Rani",
-                    position: 0
-                },
-                {
-                    user: "5rfvbgtyujmr",
-                    firstName: "Matt",
-                    lastName: "Wendzina",
-                    position: 0
-                }
-            ]
-            // users:[{user: objectID, position: 4}]
-        };
- }
-  
-    componentDidMount() {
-        const token = localStorage.getItem("token");
-        fetch(`${config.API_URI}/boards?token=${token}`, {
-          method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            }
-        })
-            .then(res => res.json())
-            //.then(data => console.log(data));
-            .then(data =>
-                this.setState(state => ({
-                    boardId: data.payload.boardId
-                }))
-            );
-    }
+        {
+          user: "7654redxcvgyu789olmnbvfr",
+          firstName: "Jonny",
+          lastName: "Lester",
+          position: 0
+        },
+        {
+          user: "plkjhgfder45678ir",
+          firstName: "Rhys",
+          lastName: "Handley",
+          position: 0
+        },
+        {
+          user: "0987654esxcvbhjk",
+          firstName: "Sahela",
+          lastName: "Rani",
+          position: 0
+        },
+        {
+          user: "5rfvbgtyujmr",
+          firstName: "Matt",
+          lastName: "Wendzina",
+          position: 0
+        }
+      ]
+      // users:[{user: objectID, position: 4}]
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                {!this.props.isLoggedIn ? (
-                    <Redirect to="/login" />
-                ) : (
-                    <div className="App">
-                        <NavBar boardId={this.state.boardId} />
-                        <div className="container">
-                            <div className="listCont">
-                                <div>
-                                    <UserList users={this.state.users} />
-                                    <Clock />
-                                </div>
-                                <div>
-                                    <StaffList users={this.state.users} />
-                                </div>
-                            </div>
-                            <div className="gridCont">
-                                <div className="Grid">
-                                    <Grid boardId={this.state.boardId} />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
+  //import DnDTest from "../DnD/DnDTest";
 
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    fetch(`${config.API_URI}/boards?token=${token}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      //.then(data => console.log(data));
+      .then(data =>
+        this.setState(state => ({
+          boardId: data.payload.boardId
+        }))
+      );
+  }
+
+  userInput = event => {
+    const { value } = event.target;
+    this.setState(
+      () => ({
+        value: value
+      }),
+      this.displayMatches
+    );
+  };
+
+  findMatches = (wordToMatch, user) => {
+    return user.filter(user => {
+      const regex = new RegExp(wordToMatch, "gi");
+      console.log(regex);
+      return user.userName.match(regex);
+    });
+  };
+
+  displayMatches = () => {
+    const matchesArray = this.findMatches(this.state.value, sampleUsers);
+    console.log(matchesArray);
+    this.setState(() => ({
+      users: matchesArray
+    }));
+  };
+
+  addUser = e => {
+    console.log(innerText);
+    const innerText = e.target.innerText;
+    this.setState(
+      () => ({
+        users2: [...this.state.users2, { user: innerText }],
+        value: ""
+      }),
+      function() {
+        console.log(this.state.users);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        {!this.props.isLoggedIn ? (
+          <Redirect to="/login" />
+        ) : (
+          <div className="App">
+            <NavBar />
+            <div className="container">
+              <div className="listCont">
+                <div>
+                  <UserList
+                    users={this.state.users}
+                    userInput={this.userInput}
+                    inputValue={this.state.value}
+                    addUser={this.addUser}
+                    user2={this.state.users2}
+                  />
+                  <Clock />
+                </div>
+                <div>
+                  <StaffList />
+                </div>
+              </div>
+              <div className="gridCont">
+                <div className="Grid">
+                  <Grid />
+                </div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Board;
